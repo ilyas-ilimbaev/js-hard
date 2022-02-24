89d7250dc5325197db19466f5a9156b956281ac3
Ильяс Илимбаев <ilyas.ilimbaev@yandex.ru>
2022-02-24 22:27:14 +0500

    Fix lesson 07

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const block = document.getElementById("week");
+const week = ['Воскресенье','Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+const day = new Date();
+
+
+week.forEach(function(item) {
+    if(item == 'Суббота' || item == 'Воскресенье') {
+        if (week[day.getDay()] == item) {
+            block.innerHTML += `<b>${item.italics()}</b></br>`;
+        } else {
+            block.innerHTML += `${item.italics()}</br>`;
+        }
+    } else if(week[day.getDay()] == item) {
+         block.innerHTML += `<b>${item}</b></br>`;
+    } else {
+        block.innerHTML += item + '</br>';
+    }
+});
